3fe44cdd89bd42237847269af5ebe031365f9aa5
shonhartman <shaun@bighuman.com>
2023-02-10 11:14:33 -0500

    working 3d render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas, useFrame } from '@react-three/fiber'
+import { MeshReflectorMaterial, BakeShadows } from '@react-three/drei'
+import { EffectComposer, Bloom, DepthOfField } from '@react-three/postprocessing'
+import { easing } from 'maath'
+import Computers, {Instances} from '../src/components/Computers'
+
+function CameraRig() {
+  useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [-1 + (state.pointer.x * state.viewport.width) / 3, (1 + state.pointer.y) / 2, 5.5], 0.5, delta)
+    state.camera.lookAt(0, 0, 0)
+  })
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas style={{width: 'auto', height: '50vw'}} shadows dpr={[1, 1.5]} camera={{ position: [-1.5, 1, 5.5], fov: 45, near: 1, far: 20 }}>
+      {/* Lights */}
+      <color attach="background" args={['black']} />
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight position={[10, 20, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+      {/* <Sphere/> */}
+      {/* Main scene */}
+        <group position={[-0, -1, 0]}>
+          {/* sketchfab model */}
+          <Instances>
+            <Computers scale={0.5} />
+          </Instances>
+          {/* Plane reflections + distance blur */}
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[50, 50]} />
+            <MeshReflectorMaterial
+              blur={[300, 30]}
+              resolution={2048}
+              mixBlur={1}
+              mixStrength={80}
+              roughness={1}
+              depthScale={1.2}
+              minDepthThreshold={0.4}
+              maxDepthThreshold={1.4}
+              color="#202020"
+              metalness={0.8}
+            />
+          </mesh>
+        </group>
+      {/* Postprocessing */}
+      <EffectComposer disableNormalPass>
+        <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={4} />
+        <DepthOfField target={[0, 0, 13]} focalLength={0.3} bokehScale={15} height={700} />
+      </EffectComposer>
+      {/* Camera movements */}
+      <CameraRig/>
+      {/* Small helper that freezes the shadows for better performance */}
+      <BakeShadows />
+    </Canvas>
     </div>
   );
 }
---
+++ src/components/Video.js
@@ -0,0 +1,29 @@
+import { Suspense, useRef } from 'react'
+import { useAspect, useVideoTexture, useTexture } from '@react-three/drei'
+
+export default function Video() {
+  const size = useAspect(180, 100)
+  return (
+    <mesh scale={size}>
+      <planeGeometry />
+      <Suspense fallback={<FallbackMaterial url="vercel.svg" />}>
+        <VideoMaterial url="PunkRockSinatra.mp4" />
+      </Suspense>
+    </mesh>
+  )
+}
+
+function VideoMaterial({ url }) {
+  const texture = useVideoTexture(url)
+  return (
+    <>
+      <meshBasicMaterial map={texture} toneMapped={false} />
+    </>
+  )
+}
+
+
+function FallbackMaterial({ url }) {
+  const texture = useTexture(url)
+  return <meshBasicMaterial map={texture} toneMapped={false} />
+}
